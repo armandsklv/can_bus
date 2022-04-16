41e635fccde4affc43df271ba35d673681dd1a4e
ArmandsK <armandsklecetnikovs@gmail.com>
2022-04-16 13:00:17 +0300

    Changed how timestamps are written when writing CAN bus data.

--- analyzing_can_data.py
+++ analyzing_can_data.py
@@ -5,21 +5,24 @@ import seaborn as sns
 from collections import Counter
 
 # Reading the recorded CAN bus data from a CSV file
-can_sample = open("C:/Users/mithr/Desktop/18.03.2022/can_high_speed_run.csv", "r")
+can_sample = open("C:/Users/mithr/Desktop/Magistrs/18.03.2022/can_low_speed_run.csv", "r")
 dataFrame = pd.read_csv(can_sample)
 can_sample.close()
+
+can_sample2 = open("C:/Users/mithr/Desktop/Magistrs/18.03.2022/FR_low_speed_run_data.csv", "r")
+dataFrame3 = pd.read_csv(can_sample2)
+can_sample2.close()
 
 # Concatenating all bytes into a single Data value
 dataFrame['Data'] = dataFrame['Byte1'] + dataFrame['Byte2'] + dataFrame['Byte3'] + dataFrame['Byte4'] +\
                     dataFrame['Byte5'] + dataFrame['Byte6'] + dataFrame['Byte7'] + dataFrame['Byte8']
 
-print(dataFrame['Data'])
 id_vals = list(dataFrame.ID.unique())  # finding unique ID's in the recorded data
-data_vals = list(dataFrame.Data.unique())  # Finding unique Data values in the recorded data
+# data_vals = list(dataFrame.Data.unique())  # Finding unique Data values in the recorded data
 
-print("There are " + str(len(dataFrame)) + " rows in this sample")
-print("There are " + str(len(data_vals)) + " unique Data values in this sample")
-print("There are " + str(len(id_vals)) + " unique ID values in this sample")
+# print("There are " + str(len(dataFrame)) + " rows in this sample")
+# print("There are " + str(len(data_vals)) + " unique Data values in this sample")
+# print("There are " + str(len(id_vals)) + " unique ID values in this sample")
 
 # Counting ID usage to determine the most common ID's
 id_counter = Counter(dataFrame.ID)
@@ -27,40 +30,65 @@ id_counter = Counter(dataFrame.ID)
 print(id_counter.most_common(23))
 
 # Most common Data values
-data_counter = Counter(dataFrame.Data)
-data_counter.most_common(10)
+# data_counter = Counter(dataFrame.Data)
+# data_counter.most_common(10)
 
 # Selecting subsets of data and bytes by specific ID, trying to find some variances in the data and byte values
-df2 = dataFrame[dataFrame['ID'] == '0x132C07C']
+df2 = pd.DataFrame()
+df2 = df2.append(dataFrame[dataFrame['ID'] == '0x1213FFC'], ignore_index=True)
+
 df2[['Byte1', 'Byte2', 'Byte3', 'Byte4', 'Byte5', 'Byte6', 'Byte7', 'Byte8']] = df2[['Byte1', 'Byte2', 'Byte3', 'Byte4', 'Byte5', 'Byte6', 'Byte7', 'Byte8']].applymap(int, base=16)
+df2['Byte8'] = df2['Byte8']
 
-fig, axs = plt.subplots(4, 2)
-axs[0, 0].plot(df2.Timestamp, df2.Byte1)
-axs[0, 0].set_title('Byte1')
-axs[1, 0].plot(df2.Timestamp, df2.Byte2)
-axs[1, 0].set_title('Byte2')
-axs[2, 0].plot(df2.Timestamp, df2.Byte3)
-axs[2, 0].set_title('Byte3')
-axs[3, 0].plot(df2.Timestamp, df2.Byte4)
-axs[3, 0].set_title('Byte4')
-axs[0, 1].plot(df2.Timestamp, df2.Byte5)
-axs[0, 1].set_title('Byte5')
-axs[1, 1].plot(df2.Timestamp, df2.Byte6)
-axs[1, 1].set_title('Byte6')
-axs[2, 1].plot(df2.Timestamp, df2.Byte7)
-axs[2, 1].set_title('Byte7')
-axs[3, 1].plot(df2.Timestamp, df2.Byte8)
-axs[3, 1].set_title('Byte8')
 
-for ax in axs.flat:
-    ax.set(xlabel='Time', ylabel='Byte value')
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(df2[['Timestamp', 'Byte8']])
 
-plt.show()
+dataFrame3['Vehicle speed. Km/h'] = dataFrame3['Vehicle speed. Km/h'] * 4 # upscaling real values to find common values and indices in raw data
+
+commonValueIdx = np.empty((0, 2), int)
+j = 0
+for i in range(0, dataFrame3['Time'].count()):
+
+    while j < df2["Timestamp"].count():
+        if dataFrame3['Vehicle speed. Km/h'][i] == df2["Byte8"][j]:
+            commonValueIdx = np.append(commonValueIdx, np.array([[i, j]]), axis=0)
+            break
+        j = j+1
+
+print(commonValueIdx)
+# df2.plot(x=df2['Timestamp'][commonValueIdx], y="Byte8", title="Byte1 with ID=0x22C01E over time")
+# plt.show()
+
+
+# fig, axs = plt.subplots(4, 2)
+# axs[0, 0].plot(df2.Timestamp, df2.Byte1)
+# axs[0, 0].set_title('Byte1')
+# axs[1, 0].plot(df2.Timestamp, df2.Byte2)
+# axs[1, 0].set_title('Byte2')
+# axs[2, 0].plot(df2.Timestamp, df2.Byte3)
+# axs[2, 0].set_title('Byte3')
+# axs[3, 0].plot(df2.Timestamp, df2.Byte4)
+# axs[3, 0].set_title('Byte4')
+# axs[0, 1].plot(df2.Timestamp, df2.Byte5)
+# axs[0, 1].set_title('Byte5')
+# axs[1, 1].plot(df2.Timestamp, df2.Byte6)
+# axs[1, 1].set_title('Byte6')
+# axs[2, 1].plot(df2.Timestamp, df2.Byte7)
+# axs[2, 1].set_title('Byte7')
+# axs[3, 1].plot(df2.Timestamp, df2.Byte8)
+# axs[3, 1].set_title('Byte8')
+#
+#
+#
+# for ax in axs.flat:
+#     ax.set(xlabel='Time', ylabel='Byte value')
+#
+# plt.show()
 # Values of Byte 7
-#print("Amount of unique Byte1 values:")
-#print(len(df2.Byte1.unique()))
-#print("Unique values of Byte1: ")
-#print(df2.Byte1.unique())
+# print("Amount of unique Byte1 values:")
+# print(len(df2.Byte1.unique()))
+# print("Unique values of Byte1: ")
+# print(df2.Byte1.unique())
+#
 
-#df2.plot(x="Timestamp", y="Byte1", title="Byte1 with ID=0x22C01E over time")
-#plt.show()
--- read_from_serial.py
+++ read_from_serial.py
@@ -1,22 +1,26 @@
 import serial
 import csv
 import datetime
+import time
 
 ser = serial.Serial(port='COM3', baudrate=115200)
 
 csv_header = ['Timestamp', 'ID', 'Byte1', 'Byte2', 'Byte3', 'Byte4', 'Byte5', 'Byte6', 'Byte7', 'Byte8']
 
-
+# Izveido jaunu .csv failu, kurā tiks ierakstīti CAN kopnes dati un pievienojam tam galveni, jeb kolonnu nosaukumus
 with open('test_csv_'+str(datetime.datetime.now().date())+'.csv', 'w') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=csv_header)
     csv_writer.writeheader()
+    t_start = time.time()  # Sākuma laiks. Tiks izmantots kā starpība, lai laika zīmogus vērtību ierakstīšanai sāktu
+    # no nulles
+#  Sākot koda izpildi ierakstām datus.
 while True:
     with open('test_csv_'+str(datetime.datetime.now().date())+'.csv', 'a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=csv_header)
         testData = ser.readline().decode('utf-8')
         data = testData.rstrip().split(',')
         row = {
-            "Timestamp": str(datetime.datetime.now().time()),
+            "Timestamp": str(round(time.time() - t_start, 3)),
             "ID": data[0],
             "Byte1": data[1],
             "Byte2": data[2],
@@ -31,6 +35,6 @@ while True:
         csv_writer.writerow(row)
         print(row)
 
-ser.close()
-f.close()
+#ser.close()
+#f.close()
 
